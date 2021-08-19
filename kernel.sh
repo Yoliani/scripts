8de1af9e831036adc3f7cb074c1b1021d8907131
Edgardo Rafael Yoliani Cantillo <70610879+Yoliani@users.noreply.github.com>
2021-08-19 12:30:33 -0500

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,8 +1,25 @@
+echo "Entrando modo superusuario"
+su
+
+echo "Dependencias"
+apt-get install su build-essential libncurses-dev bison flex libssl-dev libelf-dev bc neofetch
+sudo apt-get update
+sudo apt-get install initramfs-tools
+sudo apt-get update
+sleep 2
+
+echo "Descargando version estable del kernel"
+
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.13.12.tar.xz
+unxz -v linux-5.6.9.tar.xz
+
+echo "Revisando las llaves"
+
 gpg --verify linux-5.13.12.tar.sign
 
 sleep 2
 
-echo "Grabi, escriba la clave publica que se le dio gpg --recv-keys (la clave): "
+echo "Grab, escriba la clave publica que se le dio gpg --recv-keys (la clave): "
 
 read key
 echo "La clave es: $key"
@@ -17,3 +34,33 @@ echo "Entrando a la carpeta donde se descomprimio el kernel"
 
 cd linux-5.13.12
 cp -v /boot/config-$(uname -r) .config
+
+make menuconfig
+
+#Se obtiene la cantidad de hilos con nproc
+sleep 5
+echo "Se empezara a compilar el kernel"
+sleep 2
+make -j $(nproc)
+
+
+echo "Instalando los modulos del kernel"
+sleep 2
+
+
+sudo make modules_install
+
+sleep 1
+echo "Terminando la instalación"
+sleep 1
+
+sudo make install
+
+echo "Actualizando el grub"
+sudo update-initramfs -c -k 5.13.12
+sudo update-grub
+
+echo "Se reiniciara en 3 segundos"
+sleep 3
+
+sudo systemctl reboot
